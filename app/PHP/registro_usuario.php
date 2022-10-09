0b68c22653223d003918497da14cfb6f94707b78
Diego <34724728+Calcoph@users.noreply.github.com>
2022-10-09 17:12:51 +0200

    Se pueden registrar usuarios

--- app/PHP/registro_usuario.php
+++ app/PHP/registro_usuario.php
@@ -1,5 +1,19 @@
 <?php
+    $hostname = "db";
+    $username = "admin";
+    $password = "test";
+    $db = "database";
+
+    $conn = mysqli_connect($hostname,$username,$password,$db);
+    if ($conn->connect_error) {
+        die("Database connection failed: " . $conn->connect_error);
+    }
+
     $usuario = $_POST["usuario"];
+    $contraseña = $_POST["pswd"];
+
+    $query = mysqli_query($conn, "INSERT INTO usuario(`Used ID`, Password) VALUES ('$usuario', '$contraseña')") or die (mysqli_error($conn));
+
     $index = str_replace('%usuario%', $usuario, file_get_contents('/var/www/html/HTML/index.html'));
     echo $index
 ?>
