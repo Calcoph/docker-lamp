74c863dade3e6833d64e66483b3a4c625621092a
Diego <34724728+Calcoph@users.noreply.github.com>
2022-10-11 16:41:28 +0200

    imagen y titulo hechos

--- app/PHP/libro.php
+++ app/PHP/libro.php
@@ -13,26 +13,31 @@
 
     $query = mysqli_query($conn, "SELECT * FROM libro WHERE `Book ID`='$titulo'")
         or die (mysqli_error($conn));
-    
+
+    if (isset($_COOKIE["username"])) {
+        $username = $_COOKIE["username"];
+    } else {
+        $username = "Iniciar Sesión";
+    }
+    $header = str_replace('%usuario%', $username, file_get_contents('/var/www/html/HTML/header_small.html'));
+    $pagina = str_replace('%header%', $header, file_get_contents('/var/www/html/HTML/pagina_libro.html'));
+
     // el libro en sí
     $imagen = 2;
     $titulo_ = 0;
-    $nota = 1;
-    while ($row = mysqli_fetch_row($query)) {
-        echo
-        "<tr>
-        <td>{$row[$imagen]}</td>
-        <td>{$row[$titulo_]}</td>
-        <td>{$row[$nota]}</td>
-        </tr>";
-    }
+    $nota = 1; // TODO: poner $datos[$nota] en alguna parte
+    $datos = mysqli_fetch_row($query);
+    $pagina  = str_replace('%ruta imagen%', $datos[$imagen], $pagina);
+    $pagina  = str_replace('%titulo%', $datos[$titulo_], $pagina);
+    $descripcion = "Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae sed vitae, tempore aspernatur saepe aliquam, delectus, possimus reprehenderit ipsa nobis reiciendis incidunt praesentium porro sapiente amet dignissimos quidem esse velit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio recusandae cum sequi voluptatem nesciunt facilis possimus explicabo illo, harum temporibus dolorem alias, ea ex quae dolorum atque quisquam! Labore, itaque? lore adipisicing elit. Sit ipsum minima ut nisi reprehenderit aliquam iste maxime quisquam, rerum, ullam sequi fuga quidem in voluptates incidunt quibusdam beatae officiis omnis?";
+    $pagina  = str_replace('%descripcion%', $descripcion, $pagina);
 
     $query = mysqli_query($conn, "SELECT * FROM capitulo WHERE `Book ID`='$titulo'")
         or die (mysqli_error($conn));
 
     // capitulos
     while ($row = mysqli_fetch_assoc($query)) {
-        echo
+        $pagina .= // TODO: Poner los capitulos en otro lado
         "<tr>
         <td>{$row["Chapter_ID"]}</td>
         <td>{$row["Book ID"]}</td>
@@ -46,11 +51,13 @@
         or die (mysqli_error($conn));
 
     while ($row = mysqli_fetch_assoc($query)) {
-        echo
+        $pagina .= // TODO: hacer los comentarios
         "<tr>
         <td>{$row["nombre comentarista"]}</td>
         <td>{$row["fecha"]}</td>
         <td>{$row["comentario"]}</td>
         </tr>";
-    }   
+    }
+
+    echo $pagina
 ?>
--- app/PHP/publicar_libro.php
+++ app/PHP/publicar_libro.php
@@ -2,20 +2,23 @@
 
 <?php
     $target_dir = "/var/www/html/uploads/";
+    $save_path = "/uploads/";
     $titulo = $_POST["titulo"];
     $texto = $_POST["texto"];
     if (file_exists($_FILES["portada_personalizada"]["tmp_name"])) {
         $target_file = $target_dir . basename($_FILES["portada_personalizada"]["name"]);
+        $save_path = $save_path . basename($_FILES["portada_personalizada"]["name"]);
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
         $files = $_FILES["portada_personalizada"]["tmp_name"];
         if (move_uploaded_file($_FILES["portada_personalizada"]["tmp_name"], $target_file)) {
-            
+            echo $target_file;
         } else {
             echo "Ha habido un error al subir la portada.";
             return;
         }
     } else {
         $target_file = $target_dir . $_POST["portada"] . ".png";
+        $save_path = $save_path . $_POST["portada"] . ".png";
     }
 
     $hostname = "db";
@@ -28,7 +31,7 @@
         die("Database connection failed: " . $conn->connect_error);
     }
 
-    $query = mysqli_query($conn, "INSERT INTO libro(`Book ID`, Nota, img) VALUES ('$titulo', 0.0, '$target_dir')") or die (mysqli_error($conn));
+    $query = mysqli_query($conn, "INSERT INTO libro(`Book ID`, Nota, img) VALUES ('$titulo', 0.0, '$save_path')") or die (mysqli_error($conn));
     $query = mysqli_query($conn, "INSERT INTO capitulo(Chapter_ID, `Book ID`, `Chapter Num`, Texto) VALUES ('Cap 0', '$titulo', 0, '$texto')") or die (mysqli_error($conn));
 
     include "../index.php";
