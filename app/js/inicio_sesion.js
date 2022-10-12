208ba92716df959a0cd9dd519ae5533dc8796e3a
Diego <34724728+Calcoph@users.noreply.github.com>
2022-10-12 20:51:15 +0200

    cerrar sesión funciona

--- app/js/inicio_sesion.js
+++ app/js/inicio_sesion.js
@@ -260,3 +260,9 @@ function datos_validos(
 
     return true
 }
+
+function cerrar_sesion() {
+    // Simplemente borramos la cookie, y decimos que expira en el pasado. De: https://www.w3schools.com/js/js_cookies.asp
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location = "/index.php"
+}
