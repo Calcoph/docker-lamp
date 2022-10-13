c0d0315745c6219bf8fd735d1a7adbf12864cf68
Diego <34724728+Calcoph@users.noreply.github.com>
2022-10-13 16:22:40 +0200

    se pueden eliminar libros

--- app/PHP/inicio_sesion.php
+++ app/PHP/inicio_sesion.php
@@ -1,8 +1,8 @@
 <?php
     // Si está logueado, tiene la cookie "username"
   if (isset($_COOKIE["username"])) {
-    // le mandamos a cambiar_datos
-    echo file_get_contents('/var/www/html/HTML/cambiar_datos.html');
+    // le mandamos a las opciones de usuario
+    echo file_get_contents('/var/www/html/HTML/opciones_usuario.html');
   } else {
     // le mandamos a inicio_sesion
     echo file_get_contents('/var/www/html/HTML/inicio_sesion.html');
