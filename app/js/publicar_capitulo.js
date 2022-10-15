dcba711b05ddee5e5b505b48b1cb42345d84defd
Diego <34724728+Calcoph@users.noreply.github.com>
2022-10-15 17:35:23 +0200

    añadidos checks antes de publicar capitulo

--- app/js/publicar_capitulo.js
+++ app/js/publicar_capitulo.js
@@ -1,4 +1,17 @@
 function publicar() {
+    let titulo = document.form_publicar_capitulo.titulo.value
+    let texto = document.form_publicar_capitulo.texto.value
+
+    if (titulo == "") {
+        window.alert("No se puede dejar el título vacío")
+        return
+    }
+
+    if (texto == "") {
+        window.alert("No se puede borrar el capítulo vacío")
+        return
+    }
+
     document.form_publicar_capitulo.submit()
 }
 
