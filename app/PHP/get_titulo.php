a84f7472a9561263e76d846ee5e956e643029d6e
Diego <34724728+Calcoph@users.noreply.github.com>
2022-10-16 16:44:43 +0200

    fix typo

--- app/PHP/get_titulo.php
+++ app/PHP/get_titulo.php
@@ -11,6 +11,6 @@
   $titulo = $_GET["titulo"];
 
   $query = mysqli_query($conn, "SELECT * FROM libro WHERE `Book ID`='$titulo'") or die (mysqli_error($conn));
-  // Devuelve la contreseña del usuario que intenta loguearse
+  // Devuelve el título, si el libro está en la base de datos. Si no devuelve un ""
   echo mysqli_fetch_array($query)["Book ID"];
 ?>
