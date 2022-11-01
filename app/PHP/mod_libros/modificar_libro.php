80f5fc24fe075c13235fae789453b447ee5939ac
Diego <34724728+Calcoph@users.noreply.github.com>
2022-11-01 18:06:59 +0100

    arreglar un último select

--- app/PHP/mod_libros/modificar_libro.php
+++ app/PHP/mod_libros/modificar_libro.php
@@ -11,8 +11,13 @@
 
     $titulo = $_GET["titulo"];
 
-    $query = mysqli_query($conn, "SELECT * FROM libro WHERE `Book ID`='$titulo'")
-        or die (mysqli_error($conn));
+    $query = mysqli_prepare($conn, "SELECT imglink, `Book ID`, Text_corto, Text_largo, Prologue FROM libro WHERE `Book ID`=?") or die (mysqli_error($conn));
+    mysqli_stmt_bind_param($query, "s", $tit);
+    $tit = $titulo;
+    mysqli_stmt_execute($query) or die (mysqli_error($conn));
+
+    mysqli_stmt_bind_result($query, $imagen, $titulo_, $descripcion, $resumen, $prologo);
+    mysqli_stmt_fetch($query);
 
     if (isset($_COOKIE["username"])) {
         $user = $_COOKIE["username"];
@@ -22,27 +27,21 @@
     $header = str_replace('%usuario%', $user, file_get_contents('/var/www/html/HTML/header_small.html'));
     $pagina = str_replace('%header%', $header, file_get_contents('/var/www/html/HTML/mod_libros/modificar_libro.html'));
 
-    $imagen = 1;
-    $titulo_ = 0;
-    $descripcion = 2;
-    $resumen = 3;
-    $prologo = 4;
-    $datos = mysqli_fetch_row($query);
     // inserta la imagen en la página
-    $pagina  = str_replace('%imagen%', $datos[$imagen], $pagina);
+    $pagina  = str_replace('%imagen%', $imagen, $pagina);
     // inserta los datos actuales del libro en la página
-    $pagina  = str_replace('%titulo%', $datos[$titulo_], $pagina);
-    $pagina  = str_replace('%descripcion%', $datos[$descripcion], $pagina);
-    $pagina  = str_replace('%resumen%', $datos[$resumen], $pagina);
-    $pagina  = str_replace('%texto%', $datos[$prologo], $pagina);
+    $pagina  = str_replace('%titulo%', $titulo_, $pagina);
+    $pagina  = str_replace('%descripcion%', $descripcion, $pagina);
+    $pagina  = str_replace('%resumen%', $resumen, $pagina);
+    $pagina  = str_replace('%texto%', $prologo, $pagina);
 
     $conn = mysqli_connect($hostname,$username,$password,$db);
     if ($conn->connect_error) {
         die("Database connection failed: " . $conn->connect_error);
     }
     $query = mysqli_prepare($conn, "SELECT `Chapter Num` FROM capitulo WHERE `Book ID`=? ORDER BY `Chapter Num` ASC") or die (mysqli_error($conn));
-    mysqli_stmt_bind_param($query, "s", $tit);
-    $tit = $titulo;
+    mysqli_stmt_bind_param($query, "s", $tit2);
+    $tit2 = $titulo;
     mysqli_stmt_execute($query) or die (mysqli_error($conn));
 
     mysqli_stmt_bind_result($query, $c_num);
