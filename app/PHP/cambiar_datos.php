d1aa5778cb53d0045a94ed6d17f7a71112391ead
Diego <34724728+Calcoph@users.noreply.github.com>
2022-11-03 20:19:01 +0100

    cambiar datos comprueba algunos datos

--- app/PHP/cambiar_datos.php
+++ app/PHP/cambiar_datos.php
@@ -9,6 +9,36 @@
         die("Database connection failed: " . $conn->connect_error);
     }
 
+    // TODO: Verificar la sesión
+
+    if (!preg_match("/[a-zA-z]+/", $_POST["nombre"])) {
+        die("Nombre no válido");
+    }
+
+    if (!preg_match("/[a-zA-z]+/", $_POST["apellido"])) {
+        die("Apellido no válido");
+    }
+
+    // TODO: validar DNI
+
+    // TODO: Validar fecha
+
+    if (!preg_match("/[0-9]{9}/", $_POST["tlf"])) {
+        die("Teléfono no válido");
+    }
+
+    if (!preg_match("/\S+@\S+\.\S+/", $_POST["email"])) {
+        die("Email no válido");
+    }
+
+    if (strlen($_POST["pswd"]) < 3) {
+        die("Contraseña demasiado corta");
+    }
+
+    if (strlen($_POST["usuario"]) < 3) {
+        die("Nombre de usuario demasiado corto");
+    }
+
     // Inserta el usuario y contraseña en la base de datos
     $query = mysqli_prepare($conn, "UPDATE usuario
                                     SET `Used ID`=?, Password=?, DNI=?, email=?, Nombre=?, Apellidos=?, Telefono=?, fecha_nacimiento=?
