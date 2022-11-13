25586aab177d98ca03296f37a96fbb322826511c
Diego <34724728+Calcoph@users.noreply.github.com>
2022-11-13 17:38:35 +0100

    automatizado cambio de contraseña de la base de datos

--- app/PHP/existe_usuario.php
+++ app/PHP/existe_usuario.php
@@ -1,7 +1,7 @@
 <?php
   $hostname = "db";
   $username = "admin";
-  $password = "test";
+  $password = file_get_contents('/var/db_pass.txt');
   $db = "database";
 
   $conn = mysqli_connect($hostname,$username,$password,$db);
