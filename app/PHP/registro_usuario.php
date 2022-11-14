941532ac5645b895dd29d3053616416d22b53b60
Diego <34724728+Calcoph@users.noreply.github.com>
2022-11-14 20:04:58 +0100

    arreglar cerrar sesión

--- app/PHP/registro_usuario.php
+++ app/PHP/registro_usuario.php
@@ -25,8 +25,8 @@
     $fnacimiento = $_POST["fnacimiento"];
     mysqli_stmt_execute($query) or die (mysqli_error($conn));
 
-    setcookie("username", $_POST["usuario"], time()+24*3600); // El inicio de sesión dura 24h
-    setcookie("pass", $pass, time()+24*3600); // El inicio de sesión dura 24h
+    setcookie("username", $_POST["usuario"], time()+24*3600, "/"); // El inicio de sesión dura 24h
+    setcookie("pass", $pass, time()+24*3600, "/"); // El inicio de sesión dura 24h
 
     // Vuelve a la página principal
     header('Location: '."/index.php");
