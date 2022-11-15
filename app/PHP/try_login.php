9f99076fba146b01c50ce4ff149b1f0fcd77447e
fran <fragonzcuriel@gmail.com>
2022-11-15 21:01:41 +0100

    logs

--- app/PHP/try_login.php
+++ app/PHP/try_login.php
@@ -32,7 +32,19 @@
             header('Location: '."/index.php");
             setcookie("username", $user, time()+30*24*60*60, "/");
             setcookie("pass", $pass, time()+30*24*60*60, "/");
+            mysqli_close($conn);
+            $conn = mysqli_connect($hostname,$username,$password,$db);
+            if ($conn->connect_error) {
+                die("Database connection failed: " . $conn->connect_error);
+            }
+            $query3=mysqli_prepare($conn,"INSERT INTO `log`( `Used ID`, `Text`, `Fecha_creacion` ) VALUES (?,?,?)") or die (mysqli_error($conn));
+            mysqli_stmt_bind_param($query3, "sss", $us, $texto, $dateact);
+            $date = time();
+            $dateact=date("Y-m-d G:i:s", $date);
+            $texto="Inicio de sesión correcto";
+            mysqli_stmt_execute($query3) or die (mysqli_error($conn));
             die();
+            
         } 
         else {
             $query2 = mysqli_prepare($conn,"UPDATE usuario SET Intentos=? WHERE `Used ID`=?") or die (mysqli_error($conn));
@@ -40,6 +52,17 @@
             $us = $user;
             $intentos2=$intentos + 1;
             mysqli_stmt_execute($query2) or die (mysqli_error($conn));
+            mysqli_close($conn);
+            $conn = mysqli_connect($hostname,$username,$password,$db);
+            if ($conn->connect_error) {
+                die("Database connection failed: " . $conn->connect_error);
+            }
+            $query3=mysqli_prepare($conn,"INSERT INTO `log`( `Used ID`, `Text`, `Fecha_creacion` ) VALUES (?,?,?)") or die (mysqli_error($conn));
+            mysqli_stmt_bind_param($query3, "sss", $us, $texto, $dateact);
+            $date = time();
+            $dateact=date("Y-m-d G:i:s", $date);
+            $texto="Intento de inicio de sesión";
+            mysqli_stmt_execute($query3) or die (mysqli_error($conn));
             $pagina = str_replace('%usuario%', $user, file_get_contents('/var/www/html/HTML/inicio_sesion_fallido.html'));
             $pagina = str_replace('%scpt%', "<script>
             window.alert('Contraseña incorrecta')
@@ -49,6 +72,17 @@
         }   
     }
     else{
+        mysqli_close($conn);
+            $conn = mysqli_connect($hostname,$username,$password,$db);
+            if ($conn->connect_error) {
+                die("Database connection failed: " . $conn->connect_error);
+            }
+            $query3=mysqli_prepare($conn,"INSERT INTO `log`( `Used ID`, `Text`, `Fecha_creacion` ) VALUES (?,?,?)") or die (mysqli_error($conn));
+            mysqli_stmt_bind_param($query3, "sss", $us, $texto, $dateact);
+            $date = time();
+            $dateact=date("Y-m-d G:i:s", $date);
+            $texto="Intento de inicio de sesión con cuenta bloqueada";
+            mysqli_stmt_execute($query3) or die (mysqli_error($conn));
         $pagina = str_replace('%usuario%', $user, file_get_contents('/var/www/html/HTML/inicio_sesion_fallido.html'));
         $pagina = str_replace('%scpt%', "<script>
         window.alert('Cuenta Bloqueada')
