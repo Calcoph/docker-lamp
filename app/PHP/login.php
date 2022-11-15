9fc44308ab89d9b62058c5832aa19cc8aa706be0
Diego <34724728+Calcoph@users.noreply.github.com>
2022-11-15 20:58:30 +0100

    get_usuario() es como login pero no fuerza a iniciar sesión

--- app/PHP/login.php
+++ app/PHP/login.php
@@ -31,4 +31,33 @@ function login() {
         die();
     }
 }
+
+function get_usuario() {
+    if (sesion_valida()) {
+        $hostname = "db";
+        $username = "admin";
+        $password = file_get_contents('/var/db_pass.txt');
+        $db = "database";
+
+        $conn = mysqli_connect($hostname,$username,$password,$db);
+        if ($conn->connect_error) {
+            die("Database connection failed: " . $conn->connect_error);
+        }
+        $query = mysqli_prepare($conn, "SELECT user_id FROM session_tokens WHERE token=?") or die (mysqli_error($conn));
+        mysqli_stmt_bind_param($query, "s", $tok);
+        $tok = $_COOKIE["session"];
+        mysqli_stmt_execute($query) or die (mysqli_error($conn));
+
+        mysqli_stmt_bind_result($query, $user);
+        mysqli_stmt_fetch($query);
+
+        if ($user != NULL) {
+            return $user;
+        } else {
+            return NULL;
+        }
+    } else {
+        return NULL;
+    }
+}
 ?>
