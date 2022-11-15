a5f0b86b87f1e51538cf3c1f3788b395a2a10af0
fran <fragonzcuriel@gmail.com>
2022-11-15 19:25:42 +0100

    limitador de intentos

--- app/PHP/try_login.php
+++ app/PHP/try_login.php
@@ -11,20 +11,50 @@
     if ($conn->connect_error) {
         die("Database connection failed: " . $conn->connect_error);
     }
-    $query = mysqli_prepare($conn, "SELECT Password FROM usuario WHERE `Used ID`=?") or die (mysqli_error($conn));
+    $query = mysqli_prepare($conn, "SELECT Password , Intentos FROM usuario WHERE `Used ID`=?") or die (mysqli_error($conn));
     mysqli_stmt_bind_param($query, "s", $us);
     $us = $user;
     mysqli_stmt_execute($query) or die (mysqli_error($conn));
-
-    mysqli_stmt_bind_result($query, $pass_correcta);
+    mysqli_stmt_bind_result($query, $pass_correcta, $intentos);
     mysqli_stmt_fetch($query);
-    if (password_verify($pass, $pass_correcta)) {
-        header('Location: '."/index.php");
-        setcookie("username", $user, time()+30*24*60*60, "/");
-        setcookie("pass", $pass, time()+30*24*60*60, "/");
-        die();
-    } else {
-        $pagina = str_replace('%usuario%', $user, file_get_contents('/var/www/html/HTML/inicio_sesion_fallido.html'));
-        echo $pagina;
+    mysqli_close($conn);
+    $conn = mysqli_connect($hostname,$username,$password,$db);
+    if ($conn->connect_error) {
+        die("Database connection failed: " . $conn->connect_error);
     }
+    if($intentos<3){
+        if (password_verify($pass, $pass_correcta)) {
+            $query2 = mysqli_prepare($conn,"UPDATE usuario SET Intentos=? WHERE `Used ID`=?") or die (mysqli_error($conn));
+            mysqli_stmt_bind_param($query2, "is", $intentos2, $us);
+            $us = $user;
+            $intentos2=0;
+            mysqli_stmt_execute($query2) or die (mysqli_error($conn));
+            header('Location: '."/index.php");
+            setcookie("username", $user, time()+30*24*60*60, "/");
+            setcookie("pass", $pass, time()+30*24*60*60, "/");
+            die();
+        } 
+        else {
+            $query2 = mysqli_prepare($conn,"UPDATE usuario SET Intentos=? WHERE `Used ID`=?") or die (mysqli_error($conn));
+            mysqli_stmt_bind_param($query2, "is", $intentos2, $us);
+            $us = $user;
+            $intentos2=$intentos + 1;
+            mysqli_stmt_execute($query2) or die (mysqli_error($conn));
+            $pagina = str_replace('%usuario%', $user, file_get_contents('/var/www/html/HTML/inicio_sesion_fallido.html'));
+            $pagina = str_replace('%scpt%', "<script>
+            window.alert('Contraseña incorrecta')
+            </script>", $pagina);
+            echo $pagina;
+            die();
+        }   
+    }
+    else{
+        $pagina = str_replace('%usuario%', $user, file_get_contents('/var/www/html/HTML/inicio_sesion_fallido.html'));
+        $pagina = str_replace('%scpt%', "<script>
+        window.alert('Cuenta Bloqueada')
+        </script>", $pagina);
+        echo $pagina;
+        die();
+    }
+
 ?>
