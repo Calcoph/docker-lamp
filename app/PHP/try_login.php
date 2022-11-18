e9fd3035383e60680f5b70c39de48c81633e4e3c
fran <fragonzcuriel@gmail.com>
2022-11-18 20:55:02 +0100

    bloqueo a los 3 intentos

--- app/PHP/try_login.php
+++ app/PHP/try_login.php
@@ -25,17 +25,22 @@
     mysqli_stmt_bind_result($query, $pass_correcta, $intentos);
     mysqli_stmt_fetch($query);
     mysqli_close($conn);
-    $conn = mysqli_connect($hostname,$username,$password,$db);
-    if ($conn->connect_error) {
-        die("Database connection failed: " . $conn->connect_error);
-    }
-
-    if (password_verify($pass, $pass_correcta)) {
-        iniciar_sesion($user);
-        mysqli_close($conn);
+    if($intentos<3){
+        if (password_verify($pass, $pass_correcta)) {
+            iniciar_sesion($user);
             $conn = mysqli_connect($hostname,$username,$password,$db);
             if ($conn->connect_error) {
                 die("Database connection failed: " . $conn->connect_error);
+            }
+            $query3 = mysqli_prepare($conn,"UPDATE usuario SET Intentos=? WHERE `Used ID`=?") or die (mysqli_error($conn));
+            mysqli_stmt_bind_param($query3, "is", $intentos3, $us);
+            $us = $user;
+            $intentos3=0;
+            mysqli_stmt_execute($query3) or die (mysqli_error($conn));
+            mysqli_close($conn);
+            $conn = mysqli_connect($hostname,$username,$password,$db);
+            if ($conn->connect_error) {
+                die("DatabaseS connection failed: " . $conn->connect_error);
             }
             $query3=mysqli_prepare($conn,"INSERT INTO `log`( `Used ID`, `Text`, `Fecha_creacion` ) VALUES (?,?,?)") or die (mysqli_error($conn));
             mysqli_stmt_bind_param($query3, "sss", $us, $texto, $dateact);
@@ -46,10 +51,19 @@
             header('Location: '."/index.php");
             die();
     } else {
-        $pagina = str_replace('%usuario%', $user, file_get_contents('/var/www/html/HTML/inicio_sesion_fallido.html'));
-        $csrf = obtener_token_csrf();
-        mysqli_close($conn);
-        $conn = mysqli_connect($hostname,$username,$password,$db);
+            $pagina = str_replace('%usuario%', $user, file_get_contents('/var/www/html/HTML/inicio_sesion_fallido.html'));
+            $csrf = obtener_token_csrf();
+            $conn = mysqli_connect($hostname,$username,$password,$db);
+            if ($conn->connect_error) {
+                die("Database connection failed: " . $conn->connect_error);
+            }
+            $query4 = mysqli_prepare($conn,"UPDATE usuario SET Intentos=? WHERE `Used ID`=?") or die (mysqli_error($conn));
+            mysqli_stmt_bind_param($query4, "is", $intentos4, $us);
+            $us = $user;
+            $intentos4=$intentos+1;
+            mysqli_stmt_execute($query4) or die (mysqli_error($conn));
+            mysqli_close($conn);
+            $conn = mysqli_connect($hostname,$username,$password,$db);
             if ($conn->connect_error) {
                 die("Database connection failed: " . $conn->connect_error);
             }
@@ -63,5 +77,18 @@
             echo $pagina;
             die();
     }
+    }
+    else{
+        $csrf = obtener_token_csrf();
+        $pagina = str_replace('%usuario%', $user, file_get_contents('/var/www/html/HTML/inicio_sesion_fallido.html'));
+        $pagina  = str_replace('%nonce%', $csrf, $pagina);
+        $pagina = str_replace('%scpt%', "<script>
+        window.alert('Cuenta Bloqueada')
+        </script>", $pagina);
+        echo $pagina;
+        die();
+
+    }
+    
 
 ?>
